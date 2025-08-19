e99d8fa45bd92945e3eb21ccde3877c126f6a217
pavel.porshnev <pavel.porshnev@osinit.com>
2025-08-19 16:45:48 +0300

    refact conftest.py

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
 import pytest
 from selenium import webdriver
 
+
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
 
 
